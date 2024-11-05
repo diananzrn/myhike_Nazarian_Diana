9aba3a849ad83964c83964af0275075f1ed7b0e6
Diana Nazarian <diananzrn@gmail.com>
2024-11-05 12:23:12 -0800

    added reviews

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyAry13BEZo53dF0FLuFvH9KbPQSIRjfhfA",
+    authDomain: "comp1800-202430-demo-e5603.firebaseapp.com",
+    projectId: "comp1800-202430-demo-e5603",
+    storageBucket: "comp1800-202430-demo-e5603.appspot.com",
+    messagingSenderId: "587093277927",
+    appId: "1:587093277927:web:9ccb670faeaf7b10433642"
 };
 
 //--------------------------------------------
